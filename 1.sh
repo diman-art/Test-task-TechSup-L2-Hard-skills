1e8654550b6e686d7a44680ea4ab953dc52b75d3
Dmitry Goryachev <goriachev.mity@yandex.ru>
2023-06-03 17:49:25 +0300

    script create

--- 1.sh
+++ 1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/GreatMedivack/files/master/list.out  
 cat list.out | grep 'Running' | awk '{print $1}' > ./$(hostname)_$(date +"%d_%m_%Y").out
-rm -r list.out
-mkdir archives
+mkdir -p archives
 tar -cvzf ./archives/$(hostname)_$(date +"%d_%m_%Y").tar.gz ./$(hostname)_$(date +"%d_%m_%Y").out
-tar -tvzf ./archives/$(hostname)_$(date +"%d_%m_%Y").tar.gz > /dev/null
-# Не должно быть никаких ошибок
+tar -tvzf ./archives/$(hostname)_$(date +"%d_%m_%Y").tar.gz > /dev/null && echo Успешное завершение работы || echo Ошибка
